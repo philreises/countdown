63cf904a5ff5fd5c918710a4f62f2914ffa5eb65
Phil Reises <philreises@gmail.com>
2022-07-27 13:41:32 +0200

    Update script.js

--- src/script.js
+++ src/script.js
@@ -4,23 +4,8 @@
         hour = minute * 60,
         day = hour * 24;
 
-  //I'm adding this section so I don't have to keep updating this pen every year :-)
-  //remove this if you don't need it
-  let today = new Date(),
-      dd = String(today.getDate()).padStart(2, "0"),
-      mm = String(today.getMonth() + 1).padStart(2, "0"),
-      yyyy = today.getFullYear(),
-      nextYear = yyyy + 1,
-      dayMonth = "09/30/",
-      birthday = dayMonth + yyyy;
   
-  today = mm + "/" + dd + "/" + yyyy;
-  if (today > birthday) {
-    birthday = dayMonth + nextYear;
-  }
-  //end
-  
-  const countDown = new Date(birthday).getTime(),
+  const countDown = new Date("07/31/2022").getTime(),
       x = setInterval(function() {    
 
         const now = new Date().getTime(),
@@ -33,7 +18,7 @@
 
         //do something later when date is reached
         if (distance < 0) {
-          document.getElementById("headline").innerText = "It's my birthday!";
+          document.getElementById("headline").innerText = "Vorfreude verpasst";
           document.getElementById("countdown").style.display = "none";
           document.getElementById("content").style.display = "block";
           clearInterval(x);
